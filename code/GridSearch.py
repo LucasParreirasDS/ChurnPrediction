51bfaf3d9b35f9430bf3c7353e07041372b332fa
Lucas Parreiras <lucas.psousa@al.infnet.edu.br>
2023-06-15 17:19:37 -0300

    final version gridsearch

--- code/GridSearch.py
+++ code/GridSearch.py
@@ -20,9 +20,8 @@ df['Gender'] = encoder.fit_transform(df['Gender'])
 df = pd.get_dummies(df, columns=['Geography', 'NumOfProducts'])
 
 # Feature Selection and split target
-x = df[['NumOfProducts_2', 'NumOfProducts_1', 'Age', 'NumOfProducts_3', 'IsActiveMember', 'Geography_Germany']]
-y = df['Exited']
-
+x = df.copy()
+y = x.pop('Exited')
 y = y.values.ravel()
 
 # Applying cross-validation
@@ -40,19 +39,19 @@ model = XGBClassifier(objective='binary:logistic', eval_metric='auc')
 
 # Create a pipeline with MinMaxScaler and XGBoost
 pipeline = Pipeline([
-    ('scaler', StandardScaler()),
+    ('scaler', MinMaxScaler()),
     ('xgb', XGBClassifier(objective='binary:logistic', eval_metric='auc'))
     ])
 
 # Define the parameter grid for GridSearchCV
 param_grid = {
-    'xgb__max_depth': [3, 6, 9],
-    'xgb__learning_rate': [0.1, 0.3, 0.5],
-    'xgb__n_estimators': [50, 100, 150]
+    'xgb__max_depth': [3, 5, 7, 10, 12],
+    'xgb__learning_rate': [0.3, 0.5, 0.7],
+    'xgb__n_estimators': [75, 100, 125, 150, 175]
 }
 
 # Create the GridSearchCV object with StratifiedKFold
-grid_search = GridSearchCV(pipeline, param_grid, cv=StratifiedKFold(n_splits=5), scoring='recall', verbose=3)
+grid_search = GridSearchCV(pipeline, param_grid, cv=kfold, scoring='recall', verbose=3)
 
 # Fit the GridSearchCV object to your data
 grid_search.fit(x, y)
